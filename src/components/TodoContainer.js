6efff341568f447f9eee72d5fe3c762c6e5c8586
mohammed elsedik rebbouh <rebbouh.mohammed.elseddik@gmail.com>
2022-07-04 22:27:09 +0100

    Add checkbox functionlity for the todo items

--- src/components/TodoContainer.js
+++ src/components/TodoContainer.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import TodosList from './TodosList';
 import Header from './Header';
+import { toHaveDisplayValue } from '@testing-library/jest-dom/dist/matchers';
 
 class TodoContainer extends Component {
   state = {
@@ -18,16 +19,42 @@ class TodoContainer extends Component {
       {
         id: 3,
         title: 'Deploy to live server',
-        completed: false,
+        completed: true,
       },
     ],
   };
+
+  handleChange = (id) => {
+    this.setState((prevState) => ({
+      todos: prevState.todos.map((todo) => {
+        if (todo.id === id) {
+          console.log('inside', { ...todo, completed: !todo.completed });
+          return {
+            ...todo,
+            completed: !todo.completed,
+          };
+        }
+        console.log('outside', todo);
+        return todo;
+      }),
+    }));
+  };
+
+  // handleChange = (id) => {
+  //   this.setState({
+  //     todo: (this.state.todos[id - 1].completed =
+  //       !this.state.todos[id - 1].completed),
+  //   });
+  // };
 
   render() {
     return (
       <div>
         <Header />
-        <TodosList todos={this.state.todos} />
+        <TodosList
+          todos={this.state.todos}
+          handleChangeProps={this.handleChange}
+        />
       </div>
     );
   }
